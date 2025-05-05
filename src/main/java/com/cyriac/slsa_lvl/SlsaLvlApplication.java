e09e8591de5f0c415f50acaeba7a9a7d0db4f367
ririlafleche <186311386+ririlafleche@users.noreply.github.com>
2025-05-05 12:43:26 +0200

    first commit

---
+++ src/main/java/com/cyriac/slsa_lvl/SlsaLvlApplication.java
@@ -0,0 +1,13 @@
+package com.cyriac.slsa_lvl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlsaLvlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlsaLvlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyriac/slsa_lvl/SlsaLvlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyriac.slsa_lvl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlsaLvlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
